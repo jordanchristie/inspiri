232010e00ccede6454d73445ddb84e404331641b
Jordan Christie <jordanchristie116@gmail.com>
2018-07-22 20:41:13 -0400

    action cleanup

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import { FETCH_USER, FETCH_EXPLORE_QUOTES } from '../constants/constants';
 
-export const fetchUser = async (dispatch) => {
-    const user = await axios.get('/api/user');
-
-    dispatch({ type: FETCH_USER, payload: user.data});
+export const fetchUser = (user) => {
+    return dispatch => {
+        axios.get('/api/user')
+            .then(data => dispatch({ type: FETCH_USER, payload: user.data}));
+    }    
 }
 
-export const fetchExploreQuotes = async(dispatch) => {
-    const res = await axios.get('http://api.forismatic.com/api/1.0/');
-    const data = await res.json();
-
-    dispatch({type: FETCH_EXPLORE_QUOTES, payload: data})
-        
-
+export const fetchExploreQuotes = () => {
+    return dispatch => {
+        axios.get('http://api.forismatic.com/api/1.0/')
+            .then(res => res.json())
+            .then(data => dispatch({type: FETCH_EXPLORE_QUOTES, payload: data}));
+    } 
 }
--- server/client/src/components/ExplorePage.js
+++ server/client/src/components/ExplorePage.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ExplorePage extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     componentDidMount() {
         this.renderQuotes();
