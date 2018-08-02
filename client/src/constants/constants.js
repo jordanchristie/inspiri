6042ec38836e31b5c2ad05c49e1e2c46cc853a97
Jordan Christie <jordanchristie116@gmail.com>
2018-08-02 17:30:32 -0400

    reducer setup and api call fix

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,4 @@
 export const FETCH_USER = 'FETCH_USER';
 export const FETCH_EXPLORE_QUOTES = 'FETCH_EXPLORE_QUOTES';
+
+export const QUOTE_URL = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&lang=en'
