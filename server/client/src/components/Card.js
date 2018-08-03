a7f61f0c9030d2da975fda081f5c26cfb8628abb
Jordan Christie <jordanchristie116@gmail.com>
2018-08-03 12:39:31 -0400

    Responsive layout

--- server/client/src/components/Card.js
+++ server/client/src/components/Card.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Card = ({author, quote}) => {
     return(
         <div className="row">
-        <div classNameName="col s12 m6">
+        <div className="col s12 ">
           <div className="card blue-grey darken-1">
             <div className="card-content white-text">
               <span className="card-title">{author}</span>
               <p>{quote}</p>
             </div>
             <div className="card-action">
-              <a href=""><i className="fa fa-plus"></i> Save to Collection</a>
+              <a href=""><i className="fa fa-plus center"></i> Save to Collection</a>
+              <a href=""><i className="fa fa-facebook right"></i></a>
+              <a href=""><i className="fa fa-twitter right"></i></a>
             </div>
           </div>
         </div>
