ac0683af4123a95ec3c0d5c2788e8efc3bfb5aaa
Jordan Christie <jordanchristie116@gmail.com>
2018-08-03 12:39:31 -0400

    Responsive layout

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const LandingPage = () => {
     return (
         <section id="landing-page" style={{ textAlign: "center"}}>
             <h1>Inspiri</h1>
             <p>Collect your inspiration.</p>
-            <Link to="/auth/facebook" className="waves-effect waves-light btn-large">
+            <a href="/auth/facebook" className="waves-effect waves-light btn-large">
                 <i className="fa fa-facebook" style={{marginRight: "10px"}}></i>
                 Login With Facebook
-            </Link>
-            <Link to="/auth/twitter" className="waves-effect waves-light btn-large">
+            </a>
+            <a href="/auth/twitter" className="waves-effect waves-light btn-large">
                 <i className="fa fa-twitter" style={{marginRight: "10px"}}></i>
                 Login With Twitter
-            </Link>
-            <Link to="/auth/google" className="waves-effect waves-light btn-large">
+            </a>
+            <a href="/auth/google" className="waves-effect waves-light btn-large">
                 <i className="fa fa-google" style={{marginRight: "10px"}}></i>
                 Login With Google
-            </Link>
+            </a>
         </section>
     )
 }
