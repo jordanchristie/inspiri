3cd0f2086b2152a79e0b24b45e66d2aba64c8e24
Jordan Christie <jordanchristie116@gmail.com>
2018-09-04 16:13:02 -0400

    Materialize sideNav implementation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 class Header extends Component {
 
+
     render() {
         const { user } = this.props;
         console.log(user)
@@ -12,20 +13,28 @@ class Header extends Component {
                 <nav>
                     <div className="nav-wrapper">
                         <Link to="/" className="brand-logo">Inspiri</Link>
-                        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-                            <i class="fa fa-bars fa-2x"></i>
-                        </a>
                         { Object.keys(user).length  ?
-                            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                                <li><Link to="/explore">Explore</Link></li>
-                                <li><Link to="/dashboard">Profile</Link></li>
-                                <li><a href="/api/logout">Logout</a></li>
-                            </ul>
+                            <Fragment>
+                                <a href="" data-target="slide-out" className="sidenav-trigger">
+                                    <i className="fa fa-bars fa-2x"></i>
+                                </a>
+                                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                    <li><Link to="/explore">Explore</Link></li>
+                                    <li><Link to="/dashboard">Profile</Link></li>
+                                    <li><a href="/api/logout">Logout</a></li>
+                                </ul>
+                            </Fragment>
                             :
                             null
                             }
-                        </div>
+                    </div>
                 </nav>
+
+                <ul class="sidenav" id="slide-out">
+                    <li><Link to="/explore">Explore</Link></li>
+                    <li><Link to="/dashboard">Profile</Link></li>
+                    <li><a href="/api/logout">Logout</a></li>
+                </ul>
             </Fragment>
         )
     }
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
     return (
         <section id="landing-page" style={{ textAlign: "center"}}>
             <h1>Inspiri</h1>
-            <p>Collect your inspiration.</p>
+            <h3>Collect your inspiration.</h3>
             <a href="/auth/facebook" className="waves-effect waves-light btn-large">
                 <i className="fa fa-facebook" style={{marginRight: "10px"}}></i>
                 Login With Facebook
