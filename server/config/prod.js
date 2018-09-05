997c2d3e54f868ca64d93d9e85350afdfa891707
Jordan Christie <jordanchristie116@gmail.com>
2018-09-05 09:57:56 -0400

    heroku integration

---
+++ server/config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: process.env.mongoURI,
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
+    facebookClientId: process.env.facebookClientId,
+    facebookClientSecret: process.env.facebookClientSecret,
+    twitterClientId: process.env.twitterClientId,
+    twitterClientSecret: process.env.twitterClientSecret,
+    cookieKey: 'irj4fby9gfv79qgcediqcncdexhqyuderfvniehrwufgq7fctrnio45wjcuhquyfcger65qgcy39141734516734135342edg'
+};
