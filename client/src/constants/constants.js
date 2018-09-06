37e96dd85c71b55d148b0e5adb21fdf8391126e9
Jordan Christie <jordanchristie116@gmail.com>
2018-09-06 10:19:43 -0400

    weird heroku fix

--- client/src/constants/constants.js
+++
@@ -1,6 +0,0 @@
-export const FETCH_USER = 'FETCH_USER';
-export const FETCH_EXPLORE_QUOTES = 'FETCH_EXPLORE_QUOTES';
-export const SAVE_QUOTE_TO_PROFILE = 'SAVE_QUOTE_TO_PROFILE';
-export const REMOVE_QUOTE_FROM_PROFILE = 'REMOVE_QUOTE_FROM_PROFILE';
-
-export const QUOTE_URL = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&lang=en'
