3591b77eb085fc57b0372c45b500a0be7e1a63d3
Jordan Christie <jordanchristie116@gmail.com>
2019-11-01 08:59:55 -0400

    Fixed twitter Oauth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,55 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import background from '../background.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import background from "../background.jpg";
 
 const Header = ({ user }) => {
-    return (
-      <>
-        <nav className="nav-wrapper">
-            <Link to="/" className="brand-logo">Inspiri</Link>
-            {  Object.keys(user).length ?
-                <>
-                    <a href="#" data-target="slide-out" className="sidenav-trigger">
-                        <i className="fa fa-bars fa-2x"></i>
-                    </a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><Link to="/explore">Explore</Link></li>
-                        <li><Link to="/dashboard">Profile</Link></li>
-                        <li><a href="/api/logout">Logout</a></li>
-                    </ul>
-                </>
-                :
-                null
-            }
-        </nav>
-        
-        { user ?
+  return (
+    <>
+      <nav className="nav-wrapper">
+        <Link to="/" className="brand-logo">
+          Inspiri
+        </Link>
+        {Object.keys(user).length ? (
+          <>
+            <span data-target="slide-out" className="sidenav-trigger">
+              <i className="fa fa-bars fa-2x"></i>
+            </span>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/explore">Explore</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Profile</Link>
+              </li>
+              <li>
+                <a href="/api/logout">Logout</a>
+              </li>
+            </ul>
+          </>
+        ) : null}
+      </nav>
+
+      {user ? (
         <ul className="sidenav" id="slide-out">
-            <li>
-                <div className="user-view">
-                    <div className="background">
-                        <img src={background} alt="background"/>
-                    </div>
-                    <img className="circle" src={user.avatar} alt="profile pic"/>
-                    <span className="white-text name">{user.fullName}</span>
-                </div>
-            </li>
-            <li><Link to="/explore">Explore</Link></li>
-            <li><Link to="/dashboard">Profile</Link></li>
-            <li><a href="/api/logout">Logout</a></li>
+          <li>
+            <div className="user-view">
+              <div className="background">
+                <img src={background} alt="background" />
+              </div>
+              <img className="circle" src={user.avatar} alt="profile pic" />
+              <span className="white-text name">{user.fullName}</span>
+            </div>
+          </li>
+          <li>
+            <Link to="/explore">Explore</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Profile</Link>
+          </li>
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
         </ul>
-        :
-        null
-        }
-    </>   
-    )
-                
-}
+      ) : null}
+    </>
+  );
+};
 
-
-const mapStateToProps = ({user}) => {
-    return { user }
-}
+const mapStateToProps = ({ user }) => {
+  return { user };
+};
 
 export default connect(mapStateToProps)(Header);
--- services/passport.js
+++ services/passport.js
@@ -1,105 +1,110 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy,
-      FacebookStrategy = require('passport-facebook').Strategy,
-      TwitterStrategy = require('passport-twitter').Strategy,
-      passport = require('passport'),
-      mongoose = require('mongoose'),
-      keys = require('../config/keys'),
-      User = mongoose.model('user');
+const GoogleStrategy = require("passport-google-oauth20").Strategy,
+  FacebookStrategy = require("passport-facebook").Strategy,
+  TwitterStrategy = require("passport-twitter").Strategy,
+  passport = require("passport"),
+  mongoose = require("mongoose"),
+  keys = require("../config/keys"),
+  User = mongoose.model("user");
 
 // Serialize & Deserialize ID
 
 passport.serializeUser((user, done) => {
-    done(null, user._id)
+  done(null, user._id);
 });
 
 passport.deserializeUser((id, done) => {
-    const userId = mongoose.Types.ObjectId(id);
-    User.findById(userId, (err, user) => {
-        done(err, user);
-    })
+  const userId = mongoose.Types.ObjectId(id);
+  User.findById(userId, (err, user) => {
+    done(err, user);
+  });
 });
 
 // Google Strategy
 
-passport.use( 
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-        // Check whether user exists
-         const existingUser = await User.findOne({ id: profile.id});
-            if (existingUser) {
-                return done(null, existingUser);
-            }
-         
-            // Create new User
-            const newUser =  await new User({ 
-                id: profile.id,
-                fullName: profile.displayName,
-                firstName: profile.name.givenName,
-                avatar: profile._json.image.url,
-                savedQuotes: []
-                }).save();
-          done(null, newUser);
-            
+      // Check whether user exists
+      const existingUser = await User.findOne({ id: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      // Create new User
+      const newUser = await new User({
+        id: profile.id,
+        fullName: profile.displayName,
+        firstName: profile.name.givenName,
+        avatar: profile._json.image.url,
+        savedQuotes: []
+      }).save();
+      done(null, newUser);
     }
-));
+  )
+);
 
 // Facebook Strategy
 
 passport.use(
-    new FacebookStrategy({
-          clientID: keys.facebookClientID,
-          clientSecret: keys.facebookClientSecret,
-          callbackURL: '/auth/facebook/callback',
-          profileFields: ['id', 'name', 'photos'],
-          proxy: true
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "name", "photos"],
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-          const existingUser = await User.findOne({ id: profile.id});
-            if (existingUser) {
-                return done(null, existingUser)
-            }    
-                // Create new User
-          const newUser = await new User({
-                id: profile.id,
-                fullName: profile.displayName,
-                firstName: profile.name.givenName,
-                avatar: profile.photos[0].value,
-                savedQuotes: []
-                }).save();
-          done(null, newUser) 
-                
+      const existingUser = await User.findOne({ id: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      // Create new User
+      const newUser = await new User({
+        id: profile.id,
+        fullName: profile.displayName,
+        firstName: profile.name.givenName,
+        avatar: profile.photos[0].value,
+        savedQuotes: []
+      }).save();
+      done(null, newUser);
     }
-));
- 
+  )
+);
+
 // Twitter Strategy
 
 passport.use(
-    new TwitterStrategy({
-        consumerKey: keys.twitterClientID,
-        consumerSecret: keys.twitterClientSecret,
-        callbackURL: 'auth/twitter/callback',
-        passReqToCallback: true
+  new TwitterStrategy(
+    {
+      consumerKey: keys.twitterClientID,
+      consumerSecret: keys.twitterClientSecret,
+      callbackURL: "/auth/twitter/callback",
+      passReqToCallback: true
     },
-    async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ id: profile.id });
+    async (req, tokenSecret, otherToken, profile, done) => {
+      console.log("TokenSecret: ", tokenSecret);
+      console.log("profile: ", profile);
 
-            if (existingUser) {
-                done (null, existingUser);
-            }
+      const existingUser = await User.findOne({ id: profile.id });
 
-        const newUser = await new User({
-            id: profile.id,
-            fullName: profile.displayName,
-            firstName: profile.name.givenName,
-            savedQuotes: []
-        }).save();
-        done(null, newUser);
+      if (existingUser) {
+        done(null, existingUser);
+      }
+
+      const newUser = await new User({
+        id: profile.id,
+        fullName: profile.name,
+        avatar: profile.profile_image_url,
+        savedQuotes: []
+      }).save();
+      //done(null, newUser);
     }
-    )
-)
-
+  )
+);
