0c6127ab6f1e4135191c2d947f4c982c9f80fe71
jordanchristie <jordanchristie116@gmail.com>
2020-07-28 22:08:50 -0400

    Changed isSaved to quoteSaved

--- server/models/Quote.js
+++ server/models/Quote.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose"),
 const quoteSchema = new Schema({
   quoteAuthor: { type: String, required: true },
   quoteText: { type: String, required: true },
-  isSaved: { type: Boolean, required: true, default: true },
+  quoteSaved: { type: Boolean, required: true, default: true },
 });
 
 module.exports = quoteSchema;
